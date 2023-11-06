38777fceb2fae6377ce98c9682f67c00e8dcf9cf
MakroCZ <marek.barinka@gmail.com>
2023-11-06 22:23:18 +0100

    Update version

--- cacheturhelper.meta.js
+++ cacheturhelper.meta.js
@@ -3,7 +3,7 @@
 // @name:no         Cacheturassistenten
 // @author          cachetur.no, thomfre, Makro
 // @namespace       http://cachetur.no/
-// @version         3.5.1.03.02
+// @version         3.5.1.03.03
 // @description     Companion script for cachetur.no
 // @description:no  Hjelper deg å legge til cacher i cachetur.no
 // @icon            https://cachetur.net/img/logo_top.png
