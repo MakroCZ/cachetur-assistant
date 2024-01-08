a261640f520a44472b9f323557ec0cf79ea7534d
MakroCZ <marek.barinka@gmail.com>
2024-01-08 20:42:20 +0100

    InitNotLoggedIn refactor

--- cacheturhelper.user.js
+++ cacheturhelper.user.js
@@ -748,54 +748,18 @@ async function ctInitNotLoggedIn() {
     if (_initialized) return;
     _ctPageHandler.ctInitNotLoggedIn();
 
-    if ($("#GClh_II_running")[0] && $("gclh_nav#ctl00_gcNavigation")[0]) {
-        ctPrependToHeader2(
-            '<li id="cachetur-header"><span class="cachetur-header-text"><a href="https://cachetur.no/" target="_blank"><img src="https://cachetur.net/img/logo_top.png" alt="cachetur.no" /> ' +
-                i18next.t("menu.notloggedin") +
-                "<br>" +
-                i18next.t("menu.deactivated") +
-                "</span></a></li>"
-        );
-        var liText = "",
-            liList = $("#ctl00_uxLoginStatus_divSignedIn li"),
-            listForRemove = [];
+    const dataToPrepend =   `<li id="cachetur-header">
+                                <span class="cachetur-header-text">
+                                    <a href="https://cachetur.no/" target="_blank">
+                                        <img src="https://cachetur.net/img/logo_top.png" alt="cachetur.no" />
+                                        i18next.t("menu.notloggedin")
+                                        <br>
+                                        i18next.t("menu.deactivated")
+                                    </a>
+                                </span>
+                            </li>`
 
-        $(liList).each(function () {
-            var text = $(this).text();
-
-            if (liText.indexOf("|" + text + "|") == -1)
-                liText += "|" + text + "|";
-            else listForRemove.push($(this));
-        });
-
-        $(listForRemove).each(function () {
-            $(this).remove();
-        });
-    } else {
-        ctPrependToHeader(
-            '<li id="cachetur-header"><span class="cachetur-header-text"><a href="https://cachetur.no/" target="_blank"><img src="https://cachetur.net/img/logo_top.png" alt="cachetur.no" /> ' +
-                i18next.t("menu.notloggedin") +
-                "<br>" +
-                i18next.t("menu.deactivated") +
-                "</span></a></li>"
-        );
-        var liText2 = "",
-            liList2 = $(".user-menu li"),
-            listForRemove2 = [];
-
-        $(liList2).each(function () {
-            var text = $(this).text();
-
-            if (liText2.indexOf("|" + text + "|") == -1)
-                liText2 += "|" + text + "|";
-            else listForRemove2.push($(this));
-        });
-
-        $(listForRemove2).each(function () {
-            $(this).remove();
-        });
-    }
-
+    ctPrependToHeader(dataToPrepend);
     _initialized = true;
 }
 
